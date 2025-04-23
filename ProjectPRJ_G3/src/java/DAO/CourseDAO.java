192dc75f3f3b7e4a9979e0b08c0b40a21b33b5f5
Lehuyy <uchizuna2004bn@gmail.com>
2025-04-24 00:37:34 +0700

    hay
    hay

--- ProjectPRJ_G3/src/java/DAO/CourseDAO.java
+++ ProjectPRJ_G3/src/java/DAO/CourseDAO.java
@@ -1,13 +1,47 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package DAO;
 
-/**
- *
- * @author le huy
- */
-public class CourseDAO {
-    
+import Dal.DBContext;
+import Model.Admin;
+import Model.Course;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class CourseDAO extends DBContext {
+
+    public List<Course> getAllCo() {
+        List<Course> list = new ArrayList<>();
+
+        String sql = "SELECT course_id AS id, course_name AS name, description AS des, department, credits, semester, "
+                + "max_students AS max, c.created_at AS date, a.admin_id AS adid, a.full_name AS fname "
+                + "FROM Courses c JOIN Admins a ON a.admin_id = c.created_by";
+
+        try (PreparedStatement stm = connection.prepareStatement(sql); ResultSet rs = stm.executeQuery()) {
+
+            while (rs.next()) {
+                Admin admin = new Admin(rs.getInt("adid"), rs.getString("fname"));
+                Course course = new Course(
+                        rs.getInt("id"),
+                        rs.getString("name"),
+                        rs.getString("des"),
+                        rs.getString("department"),
+                        rs.getInt("credits"),
+                        rs.getString("semester"),
+                        rs.getInt("max"),
+                        rs.getDate("date"),
+                        admin
+                );
+
+                list.add(course);
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(CourseDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return list;
+    }
+
 }
